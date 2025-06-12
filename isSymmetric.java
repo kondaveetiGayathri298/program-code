a50545eb335bb69029167830d2163b30aa839471
kondaveetiGayathri298 <kondaveetigay@gmail.com>
2025-06-12 10:36:01 +0530

    Create isSymmetric.java

---
+++ isSymmetric.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean check(TreeNode p,TreeNode q){
+        if(p==null && q==null) return true;
+        if(p==null || q==null) return false;
+
+        return (p.val==q.val && check(p.left,q.right) && check(p.right,q.left));
+    }
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null) return true;
+        return check(root.left,root.right);
+        
+    }
+}
