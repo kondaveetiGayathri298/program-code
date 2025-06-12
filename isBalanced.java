9f9cb93fd01ce6e87a8791f4e226d1c39b3addee
kondaveetiGayathri298 <kondaveetigay@gmail.com>
2025-06-12 10:07:24 +0530

    Create isBalanced.java

---
+++ isBalanced.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null)
+        {
+            return true;
+        }
+        int lh = height(root.left);
+        int rh = height(root.right);
+        return Math.abs(lh - rh) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+        
+    }
+    
+    public int height(TreeNode root)
+    {
+        if(root == null)
+        {
+            return 0;
+        }
+        return Math.max(height(root.left) , height(root.right)) + 1;
+    }
+}
